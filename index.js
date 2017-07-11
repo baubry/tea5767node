849bb6485e6bcd522b5eeb0d855ed0d6e64ec2e7
Benjamin Aubry <benjamin.aubry@gmail.com>
2017-07-11 19:43:12 +0000

    test

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ var TEA5767_ADDR = 0x60
 var buf=new Buffer(5);
 i2c1.i2cRead(TEA5767_ADDR, 5, buf, ()=>{
   console.log("config received "+buf.length);
-  console.log(buf[0]);
-  console.log(buf[1]);
-  console.log(buf[2]);
-  console.log(buf[3]);
-  console.log(buf[4]);
+  console.log("chip ID:"+(buf[3]+0x0f));
+
+// ### Frequency ###
+ let frequency = ((buf[0] & 0x3F) << 8) + buf[1];
+  // Determine the current frequency using the same high side formula as above
+  let cfreq=(frequency * 32768 / 4 - 225000) / 1000000;
+ console.log(cfreq);
+
+// ### Level ###
+let level = buf[3]>>4;
+console.log(level);
+
+//### Stereo ####
+let sf=buf[2]&0x80;
+console.log(sf);
+
 });
-i2c1.readByteSync(TEA5767_ADDR, CMD_ACCESS_CONFIG);
