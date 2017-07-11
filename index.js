635e3c71174f05cfe3414d3f0580731ed54842d1
benjamin <benjamin.aubry@gmail.com>
2017-07-11 21:50:57 +0200

    test

--- index.js
+++ index.js
@@ -2,23 +2,31 @@ var i2c = require('i2c-bus'),
   i2c1 = i2c.openSync(1);
 
 var TEA5767_ADDR = 0x60
-var buf=new Buffer(5);
-i2c1.i2cRead(TEA5767_ADDR, 5, buf, ()=>{
-  console.log("config received "+buf.length);
-  console.log("chip ID:"+(buf[3]+0x0f));
 
-// ### Frequency ###
- let frequency = ((buf[0] & 0x3F) << 8) + buf[1];
-  // Determine the current frequency using the same high side formula as above
-  let cfreq=(frequency * 32768 / 4 - 225000) / 1000000;
- console.log(cfreq);
-
-// ### Level ###
-let level = buf[3]>>4;
-console.log(level);
-
-//### Stereo ####
-let sf=buf[2]&0x80;
-console.log(sf);
-
+getChipId().then((id)=>{
+  console.log("Chip Id: "+id);
 });
+
+getFrequency().then((freq)=>{
+  console.log("Frequency: "+freq);
+});
+
+function getChipId(){
+  return new Promise(function(resolve,reject){
+    var buf=new Buffer(5);
+    i2c1.i2cRead(TEA5767_ADDR, 5, buf, ()=>{
+      resolve((buf[3]+0x0f));
+    });
+  });
+}
+
+function getFrequency(){
+  return new Promise(function(resolve,reject){
+    var buf=new Buffer(5);
+    i2c1.i2cRead(TEA5767_ADDR, 5, buf, ()=>{
+      let frequency = ((buf[0] & 0x3F) << 8) + buf[1];
+      let cfreq=(frequency * 32768 / 4 - 225000) / 1000000;
+      resolve(cfreq);
+    });
+  });
+}
